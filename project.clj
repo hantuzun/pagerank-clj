75cefe0d5389793cfe1793ae019a57da2916fb6f
emrehan <emrehan.tuzun@gmail.com>
2015-02-17 18:40:09 +0200

    Fix the license in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject pagerank "0.1.0-SNAPSHOT"
   :description "A Clojure implementation of the pagerank algorithm"
   :url "http://github.com/emrehan/pagerank-clj"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
